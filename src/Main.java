a41fa73d8e1df21b4738358a68a7b9f4394b4d6d
Kalkin999 <kalkin999@gmail.com>
2021-12-02 01:27:29 +0200

    такое решение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] number1 =  {1,2,5,9};
+        int[] number2 = {5,9,8,0};
+        for (int a : number1) {
+            for (int i : number2) {
+                if (a == i) {
+                    System.out.println(a);
+                    break;
+                }
+            }
+        }
     }
 }
